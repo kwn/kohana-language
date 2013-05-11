271d6294f31e2ab00a9ccc0e3c2b91c8806ec133
kwn <kwn@ascetic.pl>
2013-05-11 17:33:39 +0200

    Module init file updated

--- init.php
+++ init.php
@@ -1,18 +1,8 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-
 Route::set('language', 'language/change/<lang>', array(
         'lang' => '[a-z]{2}'
 ))->defaults(array(
     'controller' => 'language',
     'action'     => 'change',
 ));
-
-
-Route::set('backend_language', 'backend/language(/<action>)')
-    ->defaults(array(
-    'directory'  => 'backend',
-    'controller' => 'language',
-    'action'     => 'index'
-));
-
